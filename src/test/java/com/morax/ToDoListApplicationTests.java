ae6655fa4b854cdcc4275015b28799571e667c4b
morax <mora_x.163.com>
2023-06-12 16:06:03 +0800

    第一次上传

---
+++ src/test/java/com/morax/ToDoListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.morax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToDoListApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
